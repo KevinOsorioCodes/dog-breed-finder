ca4ca8ec5b59d2a98d4d435bbe48f63d477b3a96
Kevin Osorio <k.osorio@amipass.com>
2023-08-08 22:14:11 -0400

    update dependencies configurations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import { labels } from "@/utils/labels";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { labels } from "~/utils/labels";
 
 const inter = Inter({ subsets: ["latin"] });
 
